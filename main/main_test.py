ef45231fcb5cc20342585f571e431dd48b4cd58f
albertochiappasony <albertosilvio.chiappa@sony.com>
2024-08-16 10:32:22 +0000

    Update main commands

--- main/main_test.py
+++ main/main_test.py
@@ -25,7 +25,8 @@ if __name__ == "__main__":
         type=pathlib.Path,
         default="./saved_model/onlineLpTest",
     )
-    parser.add_argument("--strategy_name", type=str, default="default")
+    parser.add_argument("--strategy_name", type=str, default="onlineLp")
+    parser.add_argument("--device", type=str, default="cpu")
     args = parser.parse_args()
     run_test(
         data_path=args.data_path,
@@ -34,21 +35,22 @@ if __name__ == "__main__":
         category=args.category,
         experiment_path=args.experiment_path,
         strategy_name=args.strategy_name,
+        device=args.device,
     )
 
 """Examples
 # Best so far
 python main/main_test.py --data_path ./data/traffic/period-12.csv --budget 500 --target_cpa 8 --category 0 \
-    --experiment_path saved_model/IQL/train_003/checkpoint_5000 --strategy_name iql
+    --experiment_path saved_model/IQL/train_003/checkpoint_5000 --strategy_name iql --device cuda
 
 # Also good
 python main/main_test.py --data_path ./data/traffic/period-10.csv --budget 500 --target_cpa 8 --category 0 \
-    --experiment_path saved_model/IQL/train_002/checkpoint_5000 --strategy_name iql
+    --experiment_path saved_model/IQL/train_002/checkpoint_5000 --strategy_name iql --device cuda
 
-python main/main_test.py --data_path ./data/traffic/period-7.csv --budget 500 --target_cpa 12 --category 0 \
---experiment_path saved_model/IQL/train_003/checkpoint_5000 --strategy_name iql
+python main/main_test.py --data_path ./data/traffic/period-7.csv --budget 6000 --target_cpa 8 --category 0 \
+--experiment_path output/IQL/train_004/checkpoint_18000 --strategy_name iql --device cpu
 
-python main/main_test.py --data_path ./data/traffic/period-7.csv --budget 500 --target_cpa 12 --category 0 \
-    --experiment_path saved_model/onlineLpTest --strategy_name onlineLp
+python main/main_test.py --data_path ./data/traffic/period-7.csv --budget 3000 --target_cpa 8 --category 0 \
+    --experiment_path output/onlineLpTest --strategy_name onlineLp --device cuda
 
 """
--- main/main_test_default.py
+++ main/main_test_default.py
@@ -3,14 +3,14 @@ import torch
 import os
 import sys
 import argparse
+import pathlib
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
-from run.run_bc import run_bc
+from run.run_evaluate import run_test
 
 torch.manual_seed(1)
 np.random.seed(1)
 
 if __name__ == "__main__":
-    run_bc()
+    run_test()
