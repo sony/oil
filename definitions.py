8fd76e865540dcb3cd87eb2ba46d2a15e7861060
albertochiappasony <albertosilvio.chiappa@sony.com>
2024-08-28 14:44:48 +0000

    Add env config name to definitions

--- definitions.py
+++ definitions.py
@@ -4,3 +4,4 @@ import pathlib
 ROOT_DIR = pathlib.Path(__file__).resolve().parent
 MODEL_PATTERN = "rl_model_*_steps.zip"
 ENV_PATTERN = "rl_model_vecnormalize_*_steps.pkl"
+ENV_CONFIG_NAME = "env_config.json"
