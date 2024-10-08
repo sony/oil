6d52e2d98f67e2ce98323395d48e4264d218ca0f
albertochiappasony <albertosilvio.chiappa@sony.com>
2024-10-08 04:24:03 +0000

    Add command for transformer 35 elements training

--- online/main_train_onbc.py
+++ online/main_train_onbc.py
@@ -619,5 +619,11 @@ python online/main_train_onbc.py --num_envs 20 --batch_size 512 --num_steps 20_0
         --new_action --exp_action --out_suffix=_new_data_realistic_60_obs_resume_023 \
             --obs_type obs_60_keys --learning_rate 1e-5 --save_every 10000 --num_layers 3 \
                 --load_path output/training/ongoing/023_onbc_seed_0_new_data_realistic --checkpoint_num 3100000
+                
+python online/main_train_onbc.py --algo onbc_transformer --num_envs 20 --batch_size=20 --num_steps=10_000_000 \
+    --n_rollout_steps 192 --budget_min=1000 --budget_max=6000 --target_cpa_min=50 --target_cpa_max=150 \
+        --new_action --exp_action --out_prefix=029_ --out_suffix=_transformer_new_data_realistic \
+            --obs_type obs_35_keys_transformer --use_transformer --embed_size 64 --num_heads 4 --num_layers 4 \
+                --dim_feedforward 256 --dropout 0.1 --layer_norm_eps 1e-5 --learning_rate 1e-3 --save_every 50000
 
 """
