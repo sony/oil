8003d3cf654beffba3f4de85cb03c6cc28643df3
albertochiappasony <albertosilvio.chiappa@sony.com>
2024-12-03 02:22:00 +0000

    Save also the offline experiments

--- save_experiments.py
+++ save_experiments.py
@@ -5,7 +5,7 @@ import argparse
 from definitions import ROOT_DIR
 
 
-def extract_checkpoints(json_file, output_folder):
+def copy_experiments(json_file, output_folder):
 
     output_folder.mkdir(parents=True, exist_ok=True)
 
@@ -39,14 +39,31 @@ def extract_checkpoints(json_file, output_folder):
                         ):
                             shutil.copy2(item, dest_item)
 
+def copy_folder_contents(src_path, dest_path):
+
+    # Ensure the destination folder exists
+    dest_path.mkdir(parents=True, exist_ok=True)
+
+    # Copy each item in the source folder
+    for item in src_path.iterdir():
+        dest_item = dest_path / item.name
+        if item.is_dir():
+            # Recursively copy subdirectories
+            shutil.copytree(item, dest_item, dirs_exist_ok=True)  # Use dirs_exist_ok=True for Python 3.8+
+        else:
+            # Copy files
+            shutil.copy2(item, dest_item)
 
 if __name__ == "__main__":
 
     default_json_path = ROOT_DIR / "data" / "results"
-
+    offline_exp_path = ROOT_DIR / "output" / "offline"
     output_folder = ROOT_DIR / "saved_model" / "paper"
 
     # Extract the checkpoints
     for json_file in ["eval_official.json", "eval_final.json"]:
         json_file = default_json_path / json_file
-        extract_checkpoints(json_file, output_folder)
+        copy_experiments(json_file, output_folder)
+        copy_folder_contents(offline_exp_path, output_folder) 
+
+        
