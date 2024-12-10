382ec2d377014f5b5d5c94c6a1b8d2115de3a005
albertochiappasony <albertosilvio.chiappa@sony.com>
2024-12-10 07:56:40 +0000

    Add the official phase data to the downloads

--- download.sh
+++ download.sh
@@ -1,13 +1,27 @@
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_7-8.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_9-10.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_11-12.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_13.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_14-15.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_16-17.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_18-19.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_20-21.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_22-23.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_24-25.zip -P ./data/raw_traffic_final/
-wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_26-27.zip -P ./data/raw_traffic_final/
 
-for file in ./data/raw_traffic_final/*.zip; do unzip "$file" -d "${file%.zip}"; done
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_7-8.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_9-10.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_11-12.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_13.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_14-15.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_16-17.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_18-19.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_20-21.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_22-23.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_24-25.zip -P ./data/traffic/raw_traffic/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/autoBidding_general_track_data_period_26-27.zip -P ./data/traffic/raw_traffic/
+
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_7-8.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_9-10.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_11-12.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_13.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_14-15.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_16-17.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_18-19.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_20-21.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_22-23.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_24-25.zip -P ./data/traffic/raw_traffic_final/
+wget https://alimama-bidding-competition.oss-cn-beijing.aliyuncs.com/share/final/autoBidding_general_track_final_data_period_26-27.zip -P ./data/traffic/raw_traffic_final/
+
+for file in ./data/traffic/raw_traffic_final/*.zip; do unzip "$file" -d "${file%.zip}"; done
+for file in ./data/traffic/raw_traffic_final/*.zip; do unzip "$file" -d "${file%.zip}"; done
